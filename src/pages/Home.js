3f04b462c0724e0c7a5d1d0e4e32cfad884521ab
brunoBMonteiro <brunobilheri@hotmail.com>
2021-05-19 16:35:52 -0300

    Aprendendo estilizar com Stylesheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,21 @@ import {
     View, 
     Text,
     StyleSheet,
-    StatusBar,
+    TextInput,
+    Platform,
 } from 'react-native';
 
 export function Home(){
   return (
       <View style={styles.container}>
         <Text style={styles.title}>Welcome, Bruno</Text>
-
-       <StatusBar /> 
+        
+        <TextInput 
+            style={styles.input}
+            placeholder="New skill"
+            placeholderTextColor="#555"
+        />
+ 
       </View>
   )
 }
@@ -20,9 +26,21 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#121015',
+        paddingVertical: 70,
+        paddingHorizontal: 30,
     },
     title: {
-        color: '#FFF'
+        color: '#FFF',
+        fontSize: 24,
+        fontWeight: 'bold',
     },
+    input: {
+        backgroundColor: '#1F1E25',
+        color: '#FFF',
+        fontSize: 18,
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 7,
+    }
 });
 

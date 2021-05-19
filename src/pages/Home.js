4c3e3524e6b2f5cbc0e6eb59299711041042fdee
brunoBMonteiro <brunobilheri@hotmail.com>
2021-05-19 17:32:12 -0300

    primeira aula de estilização concluida

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import {
     StyleSheet,
     TextInput,
     Platform,
+    TouchableOpacity
 } from 'react-native';
 
 export function Home(){
@@ -17,7 +18,18 @@ export function Home(){
             placeholder="New skill"
             placeholderTextColor="#555"
         />
- 
+
+        <TouchableOpacity 
+            style={styles.button}
+            activeOpacity={0.7}
+        >
+            <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+
+        <Text style={[styles.title, { marginTop: 50 }]}>
+            My skills
+        </Text>
+
       </View>
   )
 }
@@ -41,6 +53,18 @@ const styles = StyleSheet.create({
         padding: Platform.OS === 'ios' ? 15 : 10,
         marginTop: 30,
         borderRadius: 7,
+    },
+    button: {
+        backgroundColor: '#A370F7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 17,
+        fontWeight: 'bold',
     }
 });
 
